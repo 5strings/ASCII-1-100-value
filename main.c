7212f0a233d791d974ab8ecef5a058af4dc34d8a
Eun Jae Lee <eunjaelee@hotmail.com>
2017-11-20 10:58:08 +1100

    main.c

---
+++ main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  1-100
+//
+//  Created by Eun Jae Lee on 20/11/17.
+//  Copyright © 2017 Eun Jae Lee. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+
+    int i;
+    
+    for (i=1; i<=100; i++) {
+        printf("ASCII value of %d is %c \n",i,i);
+    }
+    
+    getchar();
+   
+}
